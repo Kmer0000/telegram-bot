e902692c140e302f4378807f54359590423ae0e7
Kmer0000 <kmer8175@gmail.com>
2025-03-21 15:47:43 +0100

    Primo commit del mio bot Telegram

---
+++ bot.py
@@ -0,0 +1,31 @@
+from telegram.ext import ApplicationBuilder
+import datetime
+
+TOKEN = "7928038935:AAHKS23g8AarVYlb64qYI-z9zfpQdgD3czE"
+CHAT_ID = "-1002556426331"
+
+async def invia_sondaggio(context):
+    poll_message = await context.bot.send_poll(
+        chat_id=CHAT_ID,
+        question="Commander oggi?",
+        options=["Ci sono", "Non ci sono"],
+        is_anonymous=False,
+        allows_multiple_answers=False
+    )
+    await context.bot.pin_chat_message(
+        chat_id=poll_message.chat_id,
+        message_id=poll_message.message_id,
+        disable_notification=False
+    )
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    ora_invio = datetime.time(9, 0)
+
+    app.job_queue.run_daily(invia_sondaggio, ora_invio)
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
